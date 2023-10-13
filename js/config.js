fb0deec84616b23293861bc5fb7153bbe9d14012
super181818 <131360198+super181818@users.noreply.github.com>
2023-10-13 19:18:42 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/*-------config start-------*/
+var _wip = 'yylx.panguh5.com';  
+var _port = '1787';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10000';
+BaseUrl = 'http://yyds.panguh5.com:1998/' + _port ;
+_wport = '1997' ;
